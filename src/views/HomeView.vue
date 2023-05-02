cd871fab83193df5cef12e758b5065b487424fe5
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-02 11:06:04 -0400

    Renderizadas as imagens do grid, na view Home.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,57 @@
 <template>
   <div>
-    <h1>Home</h1>
+    <section id="hero">
+      <h1>Conheça este paraíso chamado Itacaré</h1>
+      <h3>"Aconchego, conforto, simpatia e alto astral"</h3>
+      <a href="#" class="btn">Saiba mais</a>
+    </section>
+    <section id="conheca-o-hotel">
+      <h3>Conheça o Hotel Paraíso</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing, elit. Totam hic quasi ea, inventore repellat in modi molestias, cum nemo deleniti eos enim qui fugit dicta?</p>
+      <hr>
+      <ul class="grid">
+        <li class="small">
+          <img src="../assets/itacare-01.jpg">
+        </li>
+        <li class="large">
+          <img src="../assets/itacare-02.jpg">
+        </li>
+        <li class="large">
+          <img src="../assets/itacare-03.jpg">
+        </li>
+        <li class="small">
+          <img src="../assets/itacare-04.jpg">
+        </li>
+      </ul>
+    </section>
+    <section id="caracteristicas">
+      <h3>Único hotel pé na areia de Itacaré</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, ipsum! Illum voluptates omnis quas odit delectus aut nisi a fugiat, minus deleniti tempore veniam debitis commodi enim excepturi fuga, labore!</p>
+      <hr>
+      <ul class="grid">
+        <li>
+          <i class="fas fa-map-marked-alt"></i>
+          <h4>Localização</h4>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga illo ratione praesentium rerum itaque nihil enim deleniti voluptatum consequuntur repellendus.</p>
+        </li>
+        <li>
+          <i class="fas fa-shopping-basket"></i>
+          <h4>Pacote de ofertas</h4>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga illo ratione praesentium rerum itaque nihil enim deleniti voluptatum consequuntur repellendus.</p>
+        </li>
+        <li>
+          <i class="fas fa-hotel"></i>
+          <h4>Acomodações</h4>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga illo ratione praesentium rerum itaque nihil enim deleniti voluptatum consequuntur repellendus.</p>
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 <script>
-  
+  export default {
+
+  }
 </script>
 <style scoped>
   
