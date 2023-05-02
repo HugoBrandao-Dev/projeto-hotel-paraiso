d3411a8ddbb138d8d09f722d7fe690a21b455931
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-02 11:15:02 -0400

    Adicionados estilos para o component Header.

--- src/components/header/HeaderComponent.vue
+++ src/components/header/HeaderComponent.vue
@@ -33,4 +33,41 @@
     }
   }
 </script>
-<style scoped></style>
+<style scoped>
+  #cabecalho {
+    background-color: #fff;
+    width: 100%;
+    /* Para ficar em cima do hero */
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px 50px;
+  }
+
+  #cabecalho img {
+    width: 200px;
+  }
+
+  #cabecalho nav {
+    display: flex;
+  }
+
+  #cabecalho nav li {
+    margin: 0 15px;
+  }
+
+  #cabecalho nav li:first-child {
+    margin-left: 0;
+  }
+
+  #cabecalho nav li:last-child {
+    margin-right: 0;
+  }
+
+  #cabecalho nav li a {
+    color: #191c03;
+  }
+</style>
