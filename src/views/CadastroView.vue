f456adf7005b74dc7193da64ff7bf2712bd67099
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-03 18:44:44 -0400

    Criada a estrutura e o estilo para o formulario de cadastro de um novo usuario.

--- src/views/CadastroView.vue
+++ src/views/CadastroView.vue
@@ -1,7 +1,33 @@
 <template>
   <article id="cadastro" class="mt-5 content-article">
-    <h1>Cadastrar</h1>
+    <h1>Novo usuário</h1>
     <hr>
+    <form id="form-cadastro" class="is-flex is-flex-direction-column">
+      Nome:
+      <input class="input is-normal" type="text" placeholder="Nome completo">
+      Email:
+      <input class="input is-normal" type="email" placeholder="exemplo@hotmail.com">
+      CPF:
+      <input class="input is-normal" type="text" placeholder="exemplo@hotmail.com">
+      Telefone:
+      <input class="input is-normal" type="tel" placeholder="(00) 00000-0000">
+      CEP:
+      <input class="input is-normal" type="text" placeholder="00000-000">
+      Estado:
+      <input class="input is-normal" type="text" placeholder="SP">
+      Cidade:
+      <input class="input is-normal" type="text" placeholder="Nome da cidade onde você mora">
+      Bairro:
+      <input class="input is-normal" type="text" placeholder="Nome do bairro onde você reside.">
+      Rua/Avenida:
+      <input class="input is-normal" type="text" placeholder="Rua: Nome da rua/Av. Um exemplo">
+      Nº da residência:
+      <input class="input is-normal" type="text">
+      Informações adicinais:
+      <textarea class="textarea" placeholder="Deseja nos informar de mais alguma coisa?" rows="10"></textarea>
+
+      <button class="button is-success is-align-self-flex-end">Cadastrar</button>
+    </form>
   </article>
 </template>
 
@@ -12,5 +38,12 @@
 </script>
 
 <style scoped>
-  
+  #form-cadastro {
+    width: 50%;
+    margin: 0 auto;
+  }
+
+  .input, .textarea {
+    margin-bottom: 10px;
+  }
 </style>
