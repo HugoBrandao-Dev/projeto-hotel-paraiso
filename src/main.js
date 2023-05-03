e366e47aa1003aecd9172818a878a48397b3cd89
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-03 15:08:36 -0400

    Instalado e configurado o framework CSS Bulma.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Import do framework Bulma
+import './../node_modules/bulma/css/bulma.css'
+
 Vue.config.productionTip = false
 
 new Vue({
