5cbb28aabc4d4bc7b7c368b4544d5c6c28b36fc9
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-03 16:16:26 -0400

    Criada view de cadastro de um novo usuario.

---
+++ src/views/CadastroView.vue
@@ -0,0 +1,16 @@
+<template>
+  <article id="cadastro" class="mt-5 content-article">
+    <h1>Cadastrar</h1>
+    <hr>
+  </article>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+  
+</style>
