2108d471141e9ce4fcb6029afb90b960f51f5e92
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-04 22:59:38 -0400

    Criadas estruturas e estilos da pagina de contato, como localizacao e meios de contato.

--- src/views/ContatoView.vue
+++ src/views/ContatoView.vue
@@ -2,6 +2,24 @@
   <article id="contato" class="mt-5 content-article">
     <h1>Contato</h1>
     <hr>
+    <section id="location-infos" class="is-flex is-justify-content-space-around is-align-items-center">
+      <div id="location">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15466.017526583704!2d-39.00457532196318!3d-14.282101490093888!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x73935fa91bad8f3%3A0x3412ed88f1cfe936!2zSXRhY2Fyw6ksIEJB!5e0!3m2!1spt-BR!2sbr!4v1683254085484!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+      <div id="infos">
+        <ul id="contacts">
+          <li class="contact">
+            <i class="fab fa-whatsapp-square"></i> +55 (73) 00000-0000
+          </li>
+          <li class="contact">
+            <i class="fas fa-at"></i> email_hotel@hotmail.com
+          </li>
+          <li class="contact">
+            <i class="fas fa-map-marked-alt"></i> Itacaré / BH
+          </li>
+        </ul>
+      </div>
+    </section>
   </article>
 </template>
 
@@ -12,5 +30,8 @@
 </script>
 
 <style scoped>
-  
+  #infos #contacts .contact {
+    color: #c3c63b;
+    font-size: 2rem;
+  }
 </style>
