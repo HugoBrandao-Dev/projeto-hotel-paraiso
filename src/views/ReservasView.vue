1dc9a6311d66c749936e0c78fa7b25cd4df10759
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-05 19:16:48 -0400

    Adicionada estrutura estilizada como conteudo para a view de Reservas.

--- src/views/ReservasView.vue
+++ src/views/ReservasView.vue
@@ -2,6 +2,82 @@
   <article id="reservas" class="mt-5 content-article">
     <h1>Reservas</h1>
     <hr>
+    <div class="columns">
+      <div class="filtro column is-one-third is-align-self-flex-start">
+        <div class="card">
+          <div class="card-content">
+            <p class="title">Filtros</p>
+            <p class="subtitle">
+              Jeff Atwood
+            </p>
+          </div>
+          <footer class="card-footer">
+            <button class="button is-ghost card-footer-item">
+              Aplicar
+            </button>
+            <button class="button is-ghost card-footer-item">
+              Limpar
+            </button>
+          </footer>
+        </div>
+      </div>
+      <div class="lista-reservas column columns is-flex is-flex-wrap-wrap">
+        <div class="tile is-parent column is-one-third">
+          <article class="tile is-child box">
+            <figure class="image is-4by3">
+              <img src="https://bulma.io/images/placeholders/640x480.png">
+            </figure>
+            <p class="title is-6 mt-2">Diária: R$200,00</p>
+              <button class="button is-info is-light">Acessar</button>
+          </article>
+        </div>
+        <div class="tile is-parent column is-one-third">
+          <article class="tile is-child box">
+            <figure class="image is-4by3">
+              <img src="https://bulma.io/images/placeholders/640x480.png">
+            </figure>
+            <p class="title is-6 mt-2">Diária: R$200,00</p>
+              <button class="button is-info is-light">Acessar</button>
+          </article>
+        </div>
+        <div class="tile is-parent column is-one-third">
+          <article class="tile is-child box">
+            <figure class="image is-4by3">
+              <img src="https://bulma.io/images/placeholders/640x480.png">
+            </figure>
+            <p class="title is-6 mt-2">Diária: R$200,00</p>
+              <button class="button is-info is-light">Acessar</button>
+          </article>
+        </div>
+        <div class="tile is-parent column is-one-third">
+          <article class="tile is-child box">
+            <figure class="image is-4by3">
+              <img src="https://bulma.io/images/placeholders/640x480.png">
+            </figure>
+            <p class="title is-6 mt-2">Diária: R$200,00</p>
+              <button class="button is-info is-light">Acessar</button>
+          </article>
+        </div>
+        <div class="tile is-parent column is-one-third">
+          <article class="tile is-child box">
+            <figure class="image is-4by3">
+              <img src="https://bulma.io/images/placeholders/640x480.png">
+            </figure>
+            <p class="title is-6 mt-2">Diária: R$200,00</p>
+              <button class="button is-info is-light">Acessar</button>
+          </article>
+        </div>
+        <div class="tile is-parent column is-one-third">
+          <article class="tile is-child box">
+            <figure class="image is-4by3">
+              <img src="https://bulma.io/images/placeholders/640x480.png">
+            </figure>
+            <p class="title is-6 mt-2">Diária: R$200,00</p>
+              <button class="button is-info is-light">Acessar</button>
+          </article>
+        </div>
+      </div>
+    </div>
   </article>
 </template>
 
@@ -11,4 +87,5 @@
   }
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
