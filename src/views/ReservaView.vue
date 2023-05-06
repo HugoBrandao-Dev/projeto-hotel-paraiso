8343eba0feef868227c41098f98828cee66eb250
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-06 15:34:35 -0400

    Criada view para informacoes de uma reserva.

---
+++ src/views/ReservaView.vue
@@ -0,0 +1,14 @@
+<template>
+  <article id="reserva" class="mt-5 content-article">
+    <h1>Reserva</h1>
+    <hr>
+  </article>
+</template>
+
+<script>
+  
+</script>
+
+<style scoped>
+  
+</style>
