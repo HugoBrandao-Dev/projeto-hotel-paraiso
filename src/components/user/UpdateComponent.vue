077175d0e57adf077356bdb099d4b7e3fc9c02f1
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-08 17:45:59 -0400

    Criada view para a atualizacao das informacoes da conta do cliente.

---
+++ src/components/user/UpdateComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card">
+    <header class="card-header p-3 is-justify-content-space-between">
+      <h1>Atualização de informações</h1>
+    </header>
+    <div class="card-content">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+  
+</script>
+
+<style scoped>
+  
+</style>
