3b5fa2fab18189223403c8d2a077e86ee0b9eb00
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-09 12:03:38 -0400

    Criado componente das reservas do usuario.

---
+++ src/components/user/ReservasComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card">
+    <header class="card-header">
+      Reservas
+    </header>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+  
+</style>
