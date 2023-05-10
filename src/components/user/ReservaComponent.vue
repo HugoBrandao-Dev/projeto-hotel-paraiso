bc3a67c4f7e5e79de5510d2336de9d44faa958b2
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-10 12:56:10 -0400

    Criado modal para modificacao da data de estadia da reserva do usuario.

--- src/components/user/ReservaComponent.vue
+++ src/components/user/ReservaComponent.vue
@@ -1,123 +1,152 @@
 <template>
   <div class="card">
-  <header class="card-header">
-    <p class="card-header-title">
-      Reserva #{{ this.$route.params.id }}
-    </p>
-  </header>
+    <header class="card-header">
+      <p class="card-header-title">
+        Reserva #{{ this.$route.params.id }}
+      </p>
+    </header>
 
-  <!-- Criar carousel das imagens do local -->
+    <!-- Criar carousel das imagens do local -->
 
-  <div class="card-image">
-    <figure class="image is-4by3">
-      <img src="../../assets/reservas/reserva.png" alt="Placeholder image">
-    </figure>
-  </div>
-  <div class="card-content">
-    <div class="content">
-      <article class="message is-info">
-        <div class="message-header">
-          Estadia:
-        </div>
-        <div class="message-body">
-          <div class="field">
-            <label class="label">Início da estadia:</label>
-            <div class="control">
-              00-00-0000
+    <div class="card-image">
+      <figure class="image is-4by3">
+        <img src="../../assets/reservas/reserva.png" alt="Placeholder image">
+      </figure>
+    </div>
+    <div class="card-content">
+      <div class="content">
+        <article class="message is-info">
+          <div class="message-header">
+            Estadia:
+          </div>
+          <div class="message-body">
+            <div class="field">
+              <label class="label">Início da estadia:</label>
+              <div class="control">
+                00-00-0000
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Fim da estadia:</label>
+              <div class="control">
+                00-00-0000
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Tempo total da estadia:</label>
+              <div class="control">
+                5 dias
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Custo total da estadia:</label>
+              <div class="control">
+                R$ 500,00
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Forma de pagamento:</label>
+              <div class="control">
+                5 x R$ 100,00 no Cartão de Crédito
+              </div>
+            </div>
+            <br>
+            <div class="field">
+              <label class="label">Estadia solicitada em:</label>
+              <div class="control">
+                <time datetime="2016-1-1 11:09">11:09 PM - 1 Jan 2016</time>
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Valor da diária:</label>
+              <div class="control">
+                R$ 100,00
+              </div>
             </div>
           </div>
-          <div class="field">
-            <label class="label">Fim da estadia:</label>
-            <div class="control">
-              00-00-0000
+        </article>
+        <article class="message is-info">
+          <div class="message-header">
+            Solicitante:
+          </div>
+          <div class="message-body">
+            <div class="field">
+              <label class="label">Nome:</label>
+              <div class="control">
+                Tobias de Oliveira
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">CPF:</label>
+              <div class="control">
+                000.000.000-00
+              </div>
             </div>
           </div>
-          <div class="field">
-            <label class="label">Tempo total da estadia:</label>
-            <div class="control">
-              5 dias
-            </div>
+        </article>
+        <article class="message is-info">
+          <div class="message-header">
+            Acomodação:
           </div>
-          <div class="field">
-            <label class="label">Custo total da estadia:</label>
-            <div class="control">
-              R$ 500,00
-            </div>
-          </div>
-          <div class="field">
-            <label class="label">Forma de pagamento:</label>
-            <div class="control">
-              5 x R$ 100,00 no Cartão de Crédito
-            </div>
-          </div>
-          <br>
-          <div class="field">
-            <label class="label">Estadia solicitada em:</label>
-            <div class="control">
-              <time datetime="2016-1-1 11:09">11:09 PM - 1 Jan 2016</time>
-            </div>
-          </div>
-          <div class="field">
-            <label class="label">Valor da diária:</label>
-            <div class="control">
-              R$ 100,00
-            </div>
-          </div>
-        </div>
-      </article>
-      <article class="message is-info">
-        <div class="message-header">
-          Solicitante:
-        </div>
-        <div class="message-body">
-          <div class="field">
-            <label class="label">Nome:</label>
-            <div class="control">
-              Tobias de Oliveira
-            </div>
-          </div>
-          <div class="field">
-            <label class="label">CPF:</label>
-            <div class="control">
-              000.000.000-00
-            </div>
-          </div>
-        </div>
-      </article>
-      <article class="message is-info">
-        <div class="message-header">
-          Acomodação:
-        </div>
 
-        <div class="message-body">
-          <div class="field">
-            <label class="label">Andar:</label>
-            <div class="control">
-              6º
+          <div class="message-body">
+            <div class="field">
+              <label class="label">Andar:</label>
+              <div class="control">
+                6º
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Nº do apartamento:</label>
+              <div class="control">
+                22
+              </div>
+            </div>
+            <ul><span class="is-size-4">6 cômodos</span>
+              <li>1 sala de estar.</li>
+              <li>2 quartos.</li>
+              <li>1 banheiro.</li>
+              <li>1 play room.</li>
+              <li>1 cozinha.</li>
+            </ul>
+          </div>
+        </article>
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a href="#" class="button is-primary is-light card-footer-item">Modificar estadia</a>
+      <a href="#" class="button is-danger is-light card-footer-item">Cancelar estadia</a>
+    </footer>
+    <div class="modal is-active">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Modificar estadia</p>
+          <button class="delete" aria-label="close"></button>
+        </header>
+        <section class="modal-card-body">
+          <div class="columns">
+            <div class="field column is-half">
+              <label class="label">Início da estadia:</label>
+              <div class="control">
+                <input class="input" type="datetime-local">
+              </div>
+            </div>
+            <div class="field column">
+              <label class="label">Fim da estadia:</label>
+              <div class="control">
+                <input class="input" type="datetime-local">
+              </div>
             </div>
           </div>
-          <div class="field">
-            <label class="label">Nº do apartamento:</label>
-            <div class="control">
-              22
-            </div>
-          </div>
-          <ul><span class="is-size-4">6 cômodos</span>
-            <li>1 sala de estar.</li>
-            <li>2 quartos.</li>
-            <li>1 banheiro.</li>
-            <li>1 play room.</li>
-            <li>1 cozinha.</li>
-          </ul>
-        </div>
-      </article>
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button is-success">Salvar mudança</button>
+          <button class="button is-danger">Cancelar</button>
+        </footer>
+      </div>
     </div>
   </div>
-  <footer class="card-footer">
-    <a href="#" class="button is-primary is-light card-footer-item">Modificar estadia</a>
-    <a href="#" class="button is-danger is-light card-footer-item">Cancelar estadia</a>
-  </footer>
-</div>
 </template>
 
 <script>
