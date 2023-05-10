6261a357ef1c4c0d3c14bf86220480016f0ae2f3
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-10 17:04:53 -0400

    Criada view para a area do Administrador.

---
+++ src/views/admin/AdminView.vue
@@ -0,0 +1,14 @@
+<template>
+  <article id="admin" class="mt-5 content-article">
+    <h1>Administração</h1>
+    <hr>
+  </article>
+</template>
+
+<script>
+  
+</script>
+
+<style scoped>
+  
+</style>
