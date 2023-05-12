c7213adc2ae1bd1b746a012eb88574d788cf11a8
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-12 11:15:56 -0400

    Criadas estruturas do conteudo do componente de edicao das informacoes do usuario, pela rota do Admin.

--- src/components/admin/users/EditComponent.vue
+++ src/components/admin/users/EditComponent.vue
@@ -1,15 +1,170 @@
 <template>
   <div id="user-edit">
-    <h2>Componente de edição de usuário</h2>
+    <div class="card">
+      <div class="card-content">
+        <form>
+          <fieldset>
+            <div class="field">
+              <label class="label">Nome:</label>
+              <div class="control">
+                <input class="input is-normal" type="text" placeholder="Nome completo">
+              </div>
+              <!-- <p class="help">This is a help text</p> -->
+            </div>
+
+            <div class="field">
+              <label class="label">Email:</label>
+              <div class="control">
+                <input class="input is-normal" type="email" placeholder="exemplo@hotmail.com">
+              </div>
+              <!-- <p class="help">This is a help text</p> -->
+            </div>
+
+            <div class="box columns mt-2">
+                <div class="field column is-half">
+                  <label class="label">Senha:</label>
+                  <div class="control">
+                    <input type="password" class="input is-normal"/>
+                  </div>
+                  <!-- <p class="help">This is a help text</p> -->
+                </div>
+                <div class="field column">
+                  <label class="label">Repita sua senha:</label>
+                  <div class="control">
+                    <input type="password" class="input is-normal"/>
+                  </div>
+                  <!-- <p class="help">This is a help text</p> -->
+                </div>
+            </div>
+
+            <div class="field">
+              <label class="label">CPF:</label>
+              <div class="control">
+                <imask-input class="input is-normal" type="text" :mask="masks.cpf" placeholder="000.000.000-00" />
+              </div>
+              <!-- <p class="help">This is a help text</p> -->
+            </div>
+
+            <div class="field">
+              <label class="label">Telefone:</label>
+              <div class="control">
+                <imask-input class="input is-normal" type="tel" :mask="masks.telefone" placeholder="+00 (00) 00000-0000" />
+              </div>
+              <!-- <p class="help">This is a help text</p> -->
+            </div>
+
+            <div class="field">
+              <label class="label">País/Country:</label>
+              <p class="control has-icons-left">
+                <span class="select">
+                  <select>
+                    <option selected>Brazil</option>
+                    <option>England</option>
+                    <option>China</option>
+                  </select>
+                </span>
+                <span class="icon is-small is-left">
+                  <i class="fas fa-globe"></i>
+                </span>
+              </p>
+            </div>
+
+            <div class="field">
+              <label class="label">CEP:</label>
+              <div class="control">
+                <imask-input class="input is-normal" type="text" :mask="masks.cep" placeholder="00000-000" />
+              </div>
+              <!-- <p class="help">This is a help text</p> -->
+            </div>
+
+            <div class="columns">
+              <div class="field column is-one-quarter">
+                <label class="label">Estado/State:</label>
+                <div class="control">
+                  <div class="select">
+                    <select>
+                      <option>SP</option>
+                      <option>RJ</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+
+              <div class="field column is-one-quarter">
+                <label class="label">Cidade/City:</label>
+                <div class="control">
+                  <div class="select">
+                    <select>
+                      <option>São Paulo</option>
+                      <option>São Bernardo do Campo</option>
+                    </select>
+                  </div>
+                </div>
+                <!-- <p class="help">This is a help text</p> -->
+              </div>
+            </div>
+
+            <div class="columns">
+              <div class="field column is-one-third">
+                <label class="label">Bairro:</label>
+                <div class="control">
+                  <input class="input is-normal" type="text" placeholder="Nome do bairro onde você reside.">
+                </div>
+                <!-- <p class="help">This is a help text</p> -->
+              </div>
+
+              <div class="field column is-one-third">
+                <label class="label">Rua/Avenida:</label>
+                <div class="control">
+                  <input class="input is-normal" type="text" placeholder="Nome da rua/Av. Um exemplo">
+                </div>
+                <!-- <p class="help">This is a help text</p> -->
+              </div>
+
+              <div class="field column is-one-third">
+                <label class="label">Nº da residência:</label>
+                <div class="control">
+                  <input class="input is-normal" type="text" placeholder="00000">
+                </div>
+                <!-- <p class="help">This is a help text</p> -->
+              </div>
+            </div>
+
+            <div class="field">
+              <label class="label">Informações adicionais:</label>
+              <div class="control">
+                <textarea class="textarea" placeholder="Deseja nos informar de mais alguma coisa?" rows="10"></textarea>
+              </div>
+              <!-- <p class="help">This is a help text</p> -->
+            </div>
+
+            <div class="field is-grouped is-grouped-centered">
+              <div class="buttons">
+                <button type="submit" class="button is-link">Atualizar</button>
+              </div>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import { IMaskComponent }  from 'vue-imask'
+
   export default {
     data() {
       return {
-
+        masks: {
+          cpf: '000.000.000-00',
+          telefone: '+00 (00) 00000-0000',
+          cep: '00000-000'
+        }
       }
+    },
+    components: {
+      'imask-input': IMaskComponent
     }
   }
 </script>
