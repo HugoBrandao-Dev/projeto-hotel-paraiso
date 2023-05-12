707e83cffa7dec7342237b0547018d60b3acd59f
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-11 20:52:23 -0400

    Criado componente para visualizacao das informacoes de cadastro de um cliente.

---
+++ src/components/admin/users/InfoComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="user-info">
+    <h1>Informações do usuário</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  
+</style>
