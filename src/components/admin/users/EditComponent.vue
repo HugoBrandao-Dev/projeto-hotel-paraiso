99229897402f20dbd40b4b030778a85c064d9a17
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-11 21:23:55 -0400

    Criado componente para a edicao de informacoes do usuario, pela rota do Admin.

---
+++ src/components/admin/users/EditComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="user-edit">
+    <h2>Componente de edição de usuário</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  
+</style>
