7470b761525ebfc4d2a0768bf3d5f3d37be53d95
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-13 14:03:14 -0400

    Criado conteudo para o componente de visualizacao das informacoes de um reserva.

--- src/components/admin/reservas/EditComponent.vue
+++ src/components/admin/reservas/EditComponent.vue
@@ -2,6 +2,23 @@
   <article id="reserva-edit">
     <h1>Edição de informações</h1>
     <hr>
+    <div class="tabs is-toggle">
+      <ul>
+        <li class="is-active">
+          <a>
+            <span>Cômodos</span>
+          </a>
+        </li>
+        <li>
+          <a>
+            <span>Ocupante</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <section id="selecao" class="container">
+      <router-view />
+    </section>
   </article>
 </template>
 
