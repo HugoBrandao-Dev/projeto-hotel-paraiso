b44127c4bdec7466f6c18bc41dc9dfab227cc1d9
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-14 13:17:45 -0400

    Criada estrutura de informacoes do ocupante de uma reserva, para caso haja um.

--- src/components/admin/reservas/OcupanteComponent.vue
+++ src/components/admin/reservas/OcupanteComponent.vue
@@ -1,6 +1,24 @@
 <template>
   <div id="ocupante" class="content">
     <h2>Ocupante</h2>
+    <div class="field">
+      <label class="label">Nome:</label>
+      <div class="control">
+        <p>Tobias de Oliveira</p>
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">CPF:</label>
+      <div class="control">
+        <p>000.000.000-00</p>
+      </div>
+    </div>
+    <a href="/admin/user/1" class="button is-small is-link is-outlined">
+      <span>Informação completa</span>
+      <span class="icon">
+        <i class="fas fa-arrow-circle-right"></i>
+      </span>
+    </a>
   </div>
 </template>
 
