cd92c6c213423210755c5507602cb1133445637f
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-14 13:45:40 -0400

    Criada estrutura de listagem de, para caso NAO haja um ocupante em uma determinada reserva.

--- src/components/admin/reservas/OcupanteComponent.vue
+++ src/components/admin/reservas/OcupanteComponent.vue
@@ -1,29 +1,130 @@
 <template>
   <div id="ocupante" class="content">
     <h2>Ocupante</h2>
-    <div class="field">
-      <label class="label">Nome:</label>
-      <div class="control">
-        <p>Tobias de Oliveira</p>
+    <div v-if="occupied">
+      <div class="field">
+        <label class="label">Nome:</label>
+        <div class="control">
+          <p>Tobias de Oliveira</p>
+        </div>
       </div>
-    </div>
-    <div class="field">
-      <label class="label">CPF:</label>
-      <div class="control">
-        <p>000.000.000-00</p>
+      <div class="field">
+        <label class="label">CPF:</label>
+        <div class="control">
+          <p>000.000.000-00</p>
+        </div>
       </div>
+      <a href="/admin/user/1" class="button is-small is-link is-outlined">
+        <span>Informação completa</span>
+        <span class="icon">
+          <i class="fas fa-arrow-circle-right"></i>
+        </span>
+      </a>
     </div>
-    <a href="/admin/user/1" class="button is-small is-link is-outlined">
-      <span>Informação completa</span>
-      <span class="icon">
-        <i class="fas fa-arrow-circle-right"></i>
-      </span>
-    </a>
+    <div v-else>
+      <p>N/A</p>
+      <div class="card">
+        <header class="card-header">
+          <p class="card-header-title">
+            Filtro
+          </p>
+        </header>
+        <div class="card-content">
+          <div class="content">
+            <div class="field">
+              <label class="label">Pesquisar por:</label>
+              <div class="control">
+                <label class="radio">
+                  <input type="radio" checked name="tipo-pesquisa" value="nome" v-model="searchType">
+                  Nome
+                </label>
+                <label class="radio">
+                  <input type="radio" name="tipo-pesquisa" value="cpf" v-model="searchType">
+                    CPF
+                </label>
+              </div>
+            </div>
+            <div class="field has-addons">
+              <div class="control">
+                <input v-if="searchType == 'nome'" class="input" type="text" placeholder="Tobias de Oliveira">
+                <imask-input v-else class="input" type="text" placeholder="000.000.000-00" :mask="masks.cpf"/>
+              </div>
+              <div class="control">
+                <a class="button is-info">
+                  <i class="fas fa-search"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <table class="table is-striped is-fullwidth mt-4">
+        <thead>
+          <tr>
+            <th><abbr title="Nome do usuário.">Nome</abbr></th>
+            <th><abbr title="CPF do usuário.">CPF</abbr></th>
+            <th><abbr title="Telefone de contato do usuário.">Telefone</abbr></th>
+            <th><abbr title="Opções de ação.">Ações</abbr></th>
+          </tr>
+        </thead>
+        <tfoot>
+          <tr>
+            <th><abbr title="Nome do usuário.">Nome</abbr></th>
+            <th><abbr title="CPF do usuário.">CPF</abbr></th>
+            <th><abbr title="Telefone de contato do usuário.">Telefone</abbr></th>
+            <th><abbr title="Opções de ação.">Ações</abbr></th>
+          </tr>
+        </tfoot>
+        <tbody>
+          <tr v-for="user in users" :key="user.cpf">
+            <td>{{ user.nome }}</td>
+            <td>{{ user.cpf }}</td>
+            <td>{{ user.telefone }}</td>
+            <td>
+              <form @submit.prevent="addUser()">
+                <input type="hidden" :value="user.id">
+                <button type="submit" class="button is-small is-success" title="Adicionar ocupante.">
+                  Add
+                </button>
+              </form>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
+  import { IMaskComponent }  from 'vue-imask'
 
+  export default {
+    data() {
+      return {
+        masks: {
+          cpf: '000.000.000-00'
+        },
+        users: [
+          { id: 1, nome: 'Tobias de Oliveira', cpf: '11111111111', telefone: '55119111111111', reservas_ativas: '2' },
+          { id: 2, nome: 'Dinorá de Oliveira', cpf: '22222222222', telefone: '55119222222222', reservas_ativas: '3' },
+          { id: 3, nome: 'Josias Cruz', cpf: '33333333333', telefone: '55119333333333', reservas_ativas: '1' },
+          { id: 4, nome: 'Doralice Cruz', cpf: '44444444444', telefone: '55119444444444', reservas_ativas: '1' }
+        ],
+        occupied: false,
+        searchType: 'nome'
+      }
+    },
+    methods: {
+      addUser() {
+        if (confirm('Colocar o usuário como ocupante?')) {
+          alert('Colocado com sucesso.')
+        }
+      }
+    },
+    components: {
+      'imask-input': IMaskComponent
+    }
+  }
 </script>
 
 <style scoped>
