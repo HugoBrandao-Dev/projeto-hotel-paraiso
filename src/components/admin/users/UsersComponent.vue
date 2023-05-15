f99c5d07a079f7a1dc764e0285c8ede061445fe7
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-15 19:32:32 -0400

    Criados botoes de acoes responsivos para cada usuario listado.

--- src/components/admin/users/UsersComponent.vue
+++ src/components/admin/users/UsersComponent.vue
@@ -65,7 +65,52 @@
           <td class="is-hidden-mobile">{{ user.telefone }}</td>
           <td class="is-hidden-mobile">{{ user.reservas_ativas }}</td>
           <td>
-            <div class="buttons">
+            <div class="dropdown is-right is-hidden-desktop">
+              <div class="dropdown-trigger">
+                <button class="button" aria-haspopup="true" :aria-controls="user.cpf">
+                  <span>
+                    <i class="fas fa-ellipsis-v"></i>
+                  </span>
+                </button>
+              </div>
+              <div class="dropdown-menu" :id="user.cpf" role="menu">
+                <div class="dropdown-content">
+                  <div class="dropdown-item">
+                    <div class="buttons">
+                      <router-link :to="{
+                        name: 'UserInfo',
+                        params: {
+                          id: user.id
+                        }
+                      }" class="button is-small is-info" title="Informação completa.">
+                        <span class="icon is-small">
+                          <i class="fas fa-info"></i>
+                        </span>
+                      </router-link>
+                      <router-link :to="{
+                        name: 'UserEdit',
+                        params: {
+                          id: user.id
+                        }
+                      }" class="button is-small is-warning" title="Editar usuário.">
+                        <span class="icon is-small">
+                          <i class="fas fa-edit"></i>
+                        </span>
+                      </router-link>
+                      <form @submit.prevent="confirmDeletion()">
+                        <input type="hidden" :value="user.id">
+                        <button type="submit" class="button is-small is-danger" title="Deletar usuário.">
+                          <span class="icon is-small">
+                            <i class="fas fa-trash-alt"></i>
+                          </span>
+                        </button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="buttons is-hidden-touch">
               <router-link :to="{
                 name: 'UserInfo',
                 params: {
