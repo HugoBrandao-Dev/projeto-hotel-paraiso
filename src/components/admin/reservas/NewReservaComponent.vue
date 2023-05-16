b7f17cf69756a8b89e3be1dd335782080374605a
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-15 20:39:14 -0400

    Criado componente de cadastro de nova reserva, pela rota do Admin.

---
+++ src/components/admin/reservas/NewReservaComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <article id="admin-nova-reserva">
+    <h1>Nova reserva</h1>
+    <hr>
+  </article>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  
+</style>
