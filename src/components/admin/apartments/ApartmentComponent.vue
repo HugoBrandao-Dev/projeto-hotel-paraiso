d8aa9676e92aae2b06a1e1c5b52213d28d7bb5f9
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-17 14:05:12 -0400

    Criado conteudo para o componente de visualizacao de informacoes de um apartamento.

--- src/components/admin/apartments/ApartmentComponent.vue
+++ src/components/admin/apartments/ApartmentComponent.vue
@@ -2,6 +2,50 @@
   <article>
     <h1>Apartamento</h1>
     <hr>
+    <div class="columns">
+      <div class="column is-three-fifths mx-auto">
+        <div class="card">
+          <header class="card-header">
+            <p class="card-header-title is-size-4">
+              Apartamento #{{ this.$route.params.id }}
+            </p>
+          </header>
+          <div class="card-image">
+            <figure class="image is-16by9">
+              <img src="../../../assets/reservas/reserva.png" alt="Placeholder image">
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="has-text-right">
+              <span class="tag is-success is-large">Livre</span>
+            </div>
+            <div class="field">
+              <label class="label">Andar:</label>
+              <div class="control">
+                <p>2</p>
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Apartamento:</label>
+              <div class="control">
+                <p>4</p>
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">4 Cômodos:</label>
+              <div class="control">
+                <ul>
+                  <li>1 Cozinha</li>
+                  <li>1 Sala de estar</li>
+                  <li>1 Quarto</li>
+                  <li>1 Banheiro</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </article>
 </template>
 
