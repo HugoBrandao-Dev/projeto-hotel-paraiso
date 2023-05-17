6c5d4c549842a0aeb02c036aae863db29041ebfd
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-17 16:21:35 -0400

    Criado componente para cadastro de um novo apartamento.

---
+++ src/components/admin/apartments/NewApartmentComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <article id="new-apartment">
+    <h1>Novo apartamento</h1>
+    <hr>
+    
+  </article>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  
+</style>
