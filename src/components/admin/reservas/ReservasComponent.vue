950e459f8cced94832e636680addf107a33b423d
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-18 11:58:31 -0400

    Modificada estrutura e estilo do filtro de busca por reservas.

--- src/components/admin/reservas/ReservasComponent.vue
+++ src/components/admin/reservas/ReservasComponent.vue
@@ -2,153 +2,155 @@
   <article id="reservas">
     <h1>Lista de reservas</h1>
     <hr>
-    <div class="filter columns mt-3">
-      <div class="card column is-two-thirds-tablet mx-auto">
-        <header class="card-header">
-          <p class="card-header-title">
-            Filtro
-          </p>
-        </header>
-        <div class="card-content">
-          <div class="content columns">
-            <div class="field column">
-              <label class="label">Andar:</label>
-              <div class="control">
-                <input type="number" class="input">
+    <div class="content">
+      <div class="columns mt-3">
+        <div class="column is-half mx-auto">
+          <div class="box">
+            <h2>Filtro</h2>
+            <div class="columns">
+              <div class="field column is-half">
+                <label class="label">Andar:</label>
+                <div class="control">
+                  <input type="number" class="input">
+                </div>
+              </div>
+              <div class="field column">
+                <label class="label">Número:</label>
+                <div class="control">
+                  <input type="number" class="input">
+                </div>
               </div>
             </div>
-            <div class="field column">
-              <label class="label">Apartamento:</label>
-              <div class="control">
-                <input type="number" class="input">
-              </div>
+            <div class="buttons is-right">
+              <button class="button is-ghost">Limpar</button>
+              <button class="button is-info">Buscar</button>
             </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div class="buttons is-right">
-      <a href="/admin/reservas/new" class="button is-link is-large">
-        Nova Reserva
-      </a>
-    </div>
+      <div class="buttons is-right">
+        <a href="/admin/reservas/new" class="button is-link is-large">
+          Nova Reserva
+        </a>
+      </div>
 
-    <div id="admin-reservas">
-      <table class="table is-striped is-fullwidth">
-        <thead>
-          <tr>
-            <th><abbr title="Nome do usuário.">Tipo</abbr></th>
-            <th><abbr title="CPF do usuário.">Status</abbr></th>
-            <th class="is-hidden-mobile"><abbr title="Telefone de contato do usuário.">Reservado por</abbr></th>
-            <th><abbr title="Opções de ação.">Ações</abbr></th>
-          </tr>
-        </thead>
-        <tfoot>
-          <tr>
-            <th><abbr title="Nome do usuário.">Tipo</abbr></th>
-            <th><abbr title="CPF do usuário.">Status</abbr></th>
-            <th class="is-hidden-mobile"><abbr title="Telefone de contato do usuário.">Reservado por</abbr></th>
-            <th><abbr title="Opções de ação.">Ações</abbr></th>
-          </tr>
-        </tfoot>
-        <tbody>
-          <tr v-for="reserva in reservas" :key="reserva.id">
-            <td>{{ reserva.tipo }}</td>
-            <td>
-              <span 
-                class="tag" 
-                :class="{
-                  'is-success': reserva.status == 'Livre',
-                  'is-danger': reserva.status == 'Ocupado'
-                }"
-              >
-                {{ reserva.status }}
-              </span>
-            </td>
-            <td class="is-hidden-mobile">
-              {{ reserva.reservado_por }}
-            </td>
-            <td>
-              <div class="dropdown is-right is-hidden-desktop">
-                <div class="dropdown-trigger">
-                  <button class="button" aria-haspopup="true" :aria-controls="reserva.id">
-                    <span>
-                      <i class="fas fa-ellipsis-v"></i>
-                    </span>
-                  </button>
-                </div>
-                <div class="dropdown-menu" :id="reserva.id" role="menu">
-                  <div class="dropdown-content">
-                    <div class="dropdown-item">
-                      <div class="buttons">
-                        <router-link :to="{
-                            name: 'Reserva_admin',
-                            params: {
-                              id: reserva.id
-                            }
-                          }" class="button is-small is-info" title="Informação completa.">
-                          <span class="icon is-small">
-                            <i class="fas fa-info"></i>
-                          </span>
-                        </router-link>
+      <div id="admin-reservas">
+        <table class="table is-striped is-fullwidth">
+          <thead>
+            <tr>
+              <th><abbr title="Nome do usuário.">Tipo</abbr></th>
+              <th><abbr title="CPF do usuário.">Status</abbr></th>
+              <th class="is-hidden-mobile"><abbr title="Telefone de contato do usuário.">Reservado por</abbr></th>
+              <th><abbr title="Opções de ação.">Ações</abbr></th>
+            </tr>
+          </thead>
+          <tfoot>
+            <tr>
+              <th><abbr title="Nome do usuário.">Tipo</abbr></th>
+              <th><abbr title="CPF do usuário.">Status</abbr></th>
+              <th class="is-hidden-mobile"><abbr title="Telefone de contato do usuário.">Reservado por</abbr></th>
+              <th><abbr title="Opções de ação.">Ações</abbr></th>
+            </tr>
+          </tfoot>
+          <tbody>
+            <tr v-for="reserva in reservas" :key="reserva.id">
+              <td>{{ reserva.tipo }}</td>
+              <td>
+                <span 
+                  class="tag" 
+                  :class="{
+                    'is-success': reserva.status == 'Livre',
+                    'is-danger': reserva.status == 'Ocupado'
+                  }"
+                >
+                  {{ reserva.status }}
+                </span>
+              </td>
+              <td class="is-hidden-mobile">
+                {{ reserva.reservado_por }}
+              </td>
+              <td>
+                <div class="dropdown is-right is-hidden-desktop">
+                  <div class="dropdown-trigger">
+                    <button class="button" aria-haspopup="true" :aria-controls="reserva.id">
+                      <span>
+                        <i class="fas fa-ellipsis-v"></i>
+                      </span>
+                    </button>
+                  </div>
+                  <div class="dropdown-menu" :id="reserva.id" role="menu">
+                    <div class="dropdown-content">
+                      <div class="dropdown-item">
+                        <div class="buttons">
                           <router-link :to="{
-                              name: 'Ocupante',
+                              name: 'Reserva_admin',
                               params: {
                                 id: reserva.id
                               }
-                            }" class="button is-small is-warning" title="Editar usuário.">
+                            }" class="button is-small is-info" title="Informação completa.">
                             <span class="icon is-small">
-                              <i class="fas fa-edit"></i>
+                              <i class="fas fa-info"></i>
                             </span>
                           </router-link>
-                          <form @submit.prevent="confirmDeletion()">
-                            <input type="hidden" :value="reserva.id">
-                            <button type="submit" class="button is-small is-danger" title="Deletar usuário.">
+                            <router-link :to="{
+                                name: 'Ocupante',
+                                params: {
+                                  id: reserva.id
+                                }
+                              }" class="button is-small is-warning" title="Editar usuário.">
                               <span class="icon is-small">
-                                <i class="fas fa-trash-alt"></i>
+                                <i class="fas fa-edit"></i>
                               </span>
-                            </button>
-                          </form>
+                            </router-link>
+                            <form @submit.prevent="confirmDeletion()">
+                              <input type="hidden" :value="reserva.id">
+                              <button type="submit" class="button is-small is-danger" title="Deletar usuário.">
+                                <span class="icon is-small">
+                                  <i class="fas fa-trash-alt"></i>
+                                </span>
+                              </button>
+                            </form>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div class="buttons is-hidden-touch">
-                <router-link :to="{
-                  name: 'Reserva_admin',
-                  params: {
-                    id: reserva.id
-                  }
-                }" class="button is-small is-info" title="Informação completa.">
-                  <span class="icon is-small">
-                    <i class="fas fa-info"></i>
-                  </span>
-                </router-link>
-                <router-link :to="{
-                  name: 'Ocupante',
-                  params: {
-                    id: reserva.id
-                  }
-                }" class="button is-small is-warning" title="Editar usuário.">
-                  <span class="icon is-small">
-                    <i class="fas fa-edit"></i>
-                  </span>
-                </router-link>
-                <form @submit.prevent="confirmDeletion()">
-                  <input type="hidden" :value="reserva.id">
-                  <button type="submit" class="button is-small is-danger" title="Deletar usuário.">
+                <div class="buttons is-hidden-touch">
+                  <router-link :to="{
+                    name: 'Reserva_admin',
+                    params: {
+                      id: reserva.id
+                    }
+                  }" class="button is-small is-info" title="Informação completa.">
                     <span class="icon is-small">
-                      <i class="fas fa-trash-alt"></i>
+                      <i class="fas fa-info"></i>
                     </span>
-                  </button>
-                </form>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+                  </router-link>
+                  <router-link :to="{
+                    name: 'Ocupante',
+                    params: {
+                      id: reserva.id
+                    }
+                  }" class="button is-small is-warning" title="Editar usuário.">
+                    <span class="icon is-small">
+                      <i class="fas fa-edit"></i>
+                    </span>
+                  </router-link>
+                  <form @submit.prevent="confirmDeletion()">
+                    <input type="hidden" :value="reserva.id">
+                    <button type="submit" class="button is-small is-danger" title="Deletar usuário.">
+                      <span class="icon is-small">
+                        <i class="fas fa-trash-alt"></i>
+                      </span>
+                    </button>
+                  </form>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </article>
 </template>
