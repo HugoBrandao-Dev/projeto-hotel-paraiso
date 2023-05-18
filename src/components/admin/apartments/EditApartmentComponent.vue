4b028390072efac42fa4c3f0cdf4da173fc4cab6
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-18 12:55:40 -0400

    Criado conteudo do componente de edicao das informacoes de uma reserva.

--- src/components/admin/apartments/EditApartmentComponent.vue
+++ src/components/admin/apartments/EditApartmentComponent.vue
@@ -2,8 +2,262 @@
   <article id="edit-apartment">
     <h1>Editar informações</h1>
     <hr>
-    <div class="content">
-      
+    <div class="columns">
+      <div class="column is-three-fifths mx-auto">
+        <div class="card">
+          <header class="card-header">
+            <p class="card-header-title is-size-4">
+              Apartamento #{{ this.$route.params.id }}
+            </p>
+          </header>
+          <div class="card-image">
+            <figure class="image is-16by9">
+              <img src="../../../assets/reservas/reserva.png" alt="Placeholder image">
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="tile is-ancestor is-centered">
+              <div class="tile is-parent">
+                <div class="tile is-child box has-background-primary is-flex is-justify-content-center is-align-items-center">
+                  <div>
+                    <label class="label is-size-5 has-text-white">Diária:</label>
+                    <div class="field-body">
+                      <div class="field is-expanded">
+                        <div class="field has-addons">
+                          <p class="control">
+                            <a class="button is-static">
+                              R$
+                            </a>
+                          </p>
+                          <p class="control is-expanded">
+                            <input type="number" class="input">
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+            </div>
+            <div class="tile is-parent">
+              <div class="tile is-child is-flex is-justify-content-center is-align-items-center">
+                <div class="file is-link has-name is-boxed">
+                  <label class="file-label">
+                    <input class="file-input" type="file" name="resume" multiple>
+                    <span class="file-cta">
+                      <span class="file-icon">
+                        <i class="fas fa-cloud-upload-alt"></i>
+                      </span>
+                      <span class="file-label">
+                        Fotos
+                      </span>
+                    </span>
+                    <span class="file-name">
+                      Screen Shot 2017-07-29 at 15.54.25.png
+                    </span>
+                  </label>
+                </div>
+              </div>
+            </div>
+            </div>
+            <div class="columns">
+              <div class="field column is-one-third">
+                <label class="label">Status:</label>
+                <div class="control">
+                  <div class="select is-full">
+                    <select>
+                      <option value="1">Livre</option>
+                      <option value="2">Ocupado</option>
+                      <option value="3">Indisponível</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <div class="field column is-one-third">
+                <label class="label">Andar:</label>
+                <div class="control">
+                  <input type="number" class="input"/>
+                </div>
+              </div>
+              <div class="field column is-one-third">
+                <label class="label">Apartamento:</label>
+                <div class="control">
+                  <input type="number" class="input"/>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="field">
+                <label class="label">Cômodos:</label>
+              </div>
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th><abbr title="Quantidade de cada cômodo">Quantidade</abbr></th>
+                    <th>Cômodo</th>
+                    <th>Ação</th>
+                  </tr>
+                </thead>
+                <tfoot>
+                  <tr>
+                    <td class="has-text-right" colspan="3">
+                      <button class="button is-primary is-small" @click="openRoomModal()">
+                        <span class="icon is-small">
+                          <i class="fas fa-plus"></i>
+                        </span>
+                      </button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="has-text-centered" colspan="3">
+                      <strong>Total de cômodos: <span class="has-text-success is-size-5">6</span></strong>
+                    </td>
+                  </tr>
+                </tfoot>
+                <tbody>
+                  <tr v-for="room in rooms" :key="room.name">
+                    <td>{{ room.amount }}</td>
+                    <td>{{ room.name }}</td>
+                    <td>
+                      <form @submit.prevent="confirmDeletion()">
+                        <input type="hidden">
+                        <button type="submit" class="button is-danger is-small" title="Deletar cômodo.">
+                          <span class="icon is-small">
+                            <i class="fas fa-minus"></i>
+                          </span>
+                        </button>
+                      </form>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <footer class="card-footer">
+            <a href="#" class="card-footer-item button is-success is-light">Salvar</a>
+            <a href="#" class="card-footer-item button is-danger is-light">Cancelar</a>
+          </footer>
+        </div>
+      </div>
+    </div>
+    <div class="modal" :class="{'is-active': modals.rooms.active}">
+      <div class="modal-background" @click="closeRoomModal()"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Novo cômodo</p>
+          <button class="delete" aria-label="close" @click="closeRoomModal()"></button>
+        </header>
+        <section class="modal-card-body">
+          <div class="field is-horizontal">
+            <div class="field-label is-normal">
+              <label class="label">Quantidade:</label>
+            </div>
+            <div class="field-body">
+              <div class="field">
+                <div class="control">
+                  <input class="input" type="text">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="columns is-align-items-center">
+            <div class="field is-horizontal column is-half">
+              <div class="field-label is-normal">
+                <label class="label">Cômodo:</label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                    <div class="select">
+                      <select :disabled="isComodoCustom">
+                        <option>Sala de estar</option>
+                        <option>Cozinha</option>
+                        <option>Quarto</option>
+                        <option>Banheiro</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="field is-horizontal">
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                    <label class="checkbox">
+                      <input type="checkbox" v-model="isComodoCustom"> Não está listado
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="field is-horizontal" v-show="isComodoCustom">
+            <div class="field-label is-normal">
+              <label class="label">Nome:</label>
+            </div>
+            <div class="field-body">
+              <div class="field">
+                <div class="control">
+                  <input class="input" type="text">
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button is-success">Add</button>
+          <button class="button is-danger" @click="closeRoomModal()">Cancelar</button>
+        </footer>
+      </div>
     </div>
   </article>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        modals: {
+          rooms: {
+            active: false
+          }
+        },
+        rooms: [
+          {
+            amount: 1,
+            name: 'Sala de estar'
+          },
+          {
+            amount: 1,
+            name: 'Cozinha'
+          },
+          {
+            amount: 2,
+            name: 'Quartos'
+          },
+          {
+            amount: 2,
+            name: 'Banheiros'
+          },
+        ]
+      }
+    },
+    methods: {
+      confirmDeletion() {
+        if (confirm('Deseja realmente excluir o cômodo?')) {
+          alert('Excluido com sucesso.')
+        }
+      },
+      openRoomModal() {
+        this.modals.rooms.active = true
+      },
+      closeRoomModal() {
+        this.modals.rooms.active = false
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  
+</style>
