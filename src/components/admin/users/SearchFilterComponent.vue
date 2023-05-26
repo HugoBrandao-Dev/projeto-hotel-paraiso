a2b7bbb19ac9eec763b50ce93ee3017e65fa0d08
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-05-26 12:03:13 -0400

    Criado componente exclusivo para o filtro de busca de usuario.

---
+++ src/components/admin/users/SearchFilterComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="column">
+    <h1>Filtro</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  
+</style>
