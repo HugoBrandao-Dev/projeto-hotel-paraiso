fa4f99e93f446ef3d39c2b1b473ac94d066cdc91
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2024-04-07 10:08:52 -0400

    Mdf estrutura dos elementos a serem informados.

--- src/components/admin/reservas/EditComponent.vue
+++ src/components/admin/reservas/EditComponent.vue
@@ -1,79 +1,115 @@
 <template>
   <article id="reserva-edit">
-    <h1>Edição de informações</h1>
+    <h1>Edição de uma reserva</h1>
     <hr>
-    <div id="ocupante" class="content">
-      <h2>Ocupante</h2>
-      <div v-if="occupied" class="is-flex is-justify-content-space-between">
-        <div id="user-infos">
-          <div class="field">
-            <label class="label">Nome:</label>
-            <div class="control">
-              <p>Tobias de Oliveira</p>
-            </div>
-          </div>
-          <div class="field">
-            <label class="label">CPF:</label>
-            <div class="control">
-              <p>000.000.000-00</p>
-            </div>
-          </div>
-          <a href="/admin/user/1" class="button is-small is-link is-outlined">
-            <span>Informação completa</span>
-            <span class="icon">
-              <i class="fas fa-arrow-circle-right"></i>
-            </span>
-          </a>
-        </div>
-        <div id="user-option">
-          <button class="button is-large is-danger" @click="removeUser()">Remover</button>
+    <div :class="{'has-background-danger': forms.reserve.iptClient.hasError}">
+      <h2 class="title is-2">Clientes</h2>
+      <div class="content">
+        <div class="columns">
+          <SearchFilterComponent @userFound="userFound($event)" />
         </div>
       </div>
-      <div v-else>
-        <p class="has-text-danger">
-          <span class="icon is-large">
-            <i class="fas fa-ban fa-lg"></i>
-          </span>
-          <strong class="has-text-danger">N/A</strong>
-          <span class="icon is-large">
-            <i class="fas fa-ban fa-lg"></i>
-          </span>
-        </p>
-        <SearchFilterComponent />
-        <table class="table is-striped is-fullwidth mt-4">
+      <div class="container is-max-desktop">
+        <table class="table is-striped is-fullwidth">
           <thead>
             <tr>
-              <th><abbr title="Nome do usuário.">Nome</abbr></th>
-              <th><abbr title="CPF do usuário.">CPF</abbr></th>
-              <th><abbr title="Telefone de contato do usuário.">Telefone</abbr></th>
-              <th><abbr title="Opções de ação.">Ações</abbr></th>
+              <th class="is-hidden-touch">Selecione</th>
+              <th>Nome</th>
+              <th><abbr title="CPF ou Número do Passaporte do cliente">CPF/NP</abbr></th>
             </tr>
           </thead>
           <tfoot>
             <tr>
-              <th><abbr title="Nome do usuário.">Nome</abbr></th>
-              <th><abbr title="CPF do usuário.">CPF</abbr></th>
-              <th><abbr title="Telefone de contato do usuário.">Telefone</abbr></th>
-              <th><abbr title="Opções de ação.">Ações</abbr></th>
+              <th class="is-hidden-touch">Selecione</th>
+              <th>Nome</th>
+              <th><abbr title="CPF ou Número do Passaporte do cliente">CPF/NP</abbr></th>
             </tr>
           </tfoot>
           <tbody>
-            <tr v-for="user in users" :key="user.cpf">
-              <td>{{ user.nome }}</td>
-              <td>{{ user.cpf }}</td>
-              <td>{{ user.telefone }}</td>
-              <td>
-                <form @submit.prevent="addUser()">
-                  <input type="hidden" :value="user.id">
-                  <button type="submit" class="button is-small is-success" title="Adicionar ocupante.">
-                    Add
-                  </button>
-                </form>
+            <tr 
+              v-for="user in users"
+              :key="user._id"
+              @click="forms.reserve.iptClient.value = user"
+              :class="{'is-selected': user._id == forms.reserve.iptClient.value._id }"
+            >
+              <td class="is-hidden-touch">
+                <input type="hidden" :value="user._id">
+                <input type="radio" name="user" :value="user._id" v-model="forms.reserve.iptClient.value._id">
               </td>
+              <td class="is-capitalized">{{ user.name }}</td>
+              <td v-if="user.cpf">{{ user.cpf }}</td>
+              <td v-else>{{ user.passportNumber }}</td>
             </tr>
           </tbody>
         </table>
       </div>
+    </div>
+    <div class="container mt-4">
+      <h2 class="title is-2">Informações</h2>
+      <div class="columns">
+        <div class="field column is-one-third">
+          <label class="label">Início da estadia:</label>
+          <div class="control">
+            <input 
+              id="iptStartDate"
+              type="date" 
+              class="input"
+              :class="{
+                'is-normal': !forms.reserve.iptStartDate.hasError,
+                'is-danger': forms.reserve.iptStartDate.hasError
+              }"
+              v-model="forms.reserve.iptStartDate.value">
+          </div>
+          <p class="help" :class="{ 'is-danger': forms.reserve.iptStartDate.hasError }">
+            {{ forms.reserve.iptStartDate.error }}
+          </p>
+        </div>
+        <div class="field column is-one-third">
+          <label class="label">Fim da estadia:</label>
+          <div class="control">
+            <input 
+              id="iptEndDate"
+              type="date" 
+              class="input"
+              :class="{
+                'is-normal': !forms.reserve.iptEndDate.hasError,
+                'is-danger': forms.reserve.iptEndDate.hasError
+              }"
+              v-model="forms.reserve.iptEndDate.value">
+          </div>
+          <p class="help" :class="{ 'is-danger': forms.reserve.iptEndDate.hasError }">
+            {{ forms.reserve.iptEndDate.error }}
+          </p>
+        </div>
+        <div class="field column is-one-third">
+          <label class="label">Status:</label>
+          <div class="control">
+            <div 
+              class="select" 
+              :class="{'is-danger': forms.reserve.iptStatus.hasError}"
+            >
+              <select v-model="forms.reserve.iptStatus.value">
+                <option 
+                  v-for="item in statusList" 
+                  :key="item" 
+                  :selected="forms.reserve.iptStatus.value"
+                >
+                  {{ item }}
+                </option>
+              </select>
+            </div>
+          </div>
+            <p 
+              class="help" 
+              :class="{'is-danger': forms.reserve.iptStatus.hasError}"
+            >
+              {{ forms.reserve.iptStatus.error }}
+            </p>
+        </div>
+      </div>
+    </div>
+    <div class="buttons is-right">
+      <button class="button is-link is-large" @click="updateReserve()">Atualizar</button>
     </div>
   </article>
 </template>
@@ -82,19 +118,87 @@
   import SearchFilterComponent from '../users/SearchFilterComponent'
 
   export default {
-    data() {
-      return {
-        users: [
-          { id: 1, nome: 'Tobias de Oliveira', cpf: '11111111111', telefone: '55119111111111', reservas_ativas: '2' },
-          { id: 2, nome: 'Dinorá de Oliveira', cpf: '22222222222', telefone: '55119222222222', reservas_ativas: '3' },
-          { id: 3, nome: 'Josias Cruz', cpf: '33333333333', telefone: '55119333333333', reservas_ativas: '1' },
-          { id: 4, nome: 'Doralice Cruz', cpf: '44444444444', telefone: '55119444444444', reservas_ativas: '1' }
-        ],
-        occupied: false,
-      }
-    },
     components: {
       SearchFilterComponent
+    },
+    data() {
+      return {
+        statusList: ['reservado', 'ocupado'],
+        users: [
+          {
+            _id: '1',
+            name: 'Tobias de Oliveira',
+            cpf: '11111111111',
+            phoneCode: '254',
+            phoneNumber: '55119111111111'
+          },
+          {
+            _id: '2',
+            name: 'Dinorá de Oliveira',
+            cpf: '22222222222',
+            phoneCode: '254',
+            phoneNumber: '55119222222222'
+          },
+          {
+            _id: '3',
+            name: 'Josias Cruz',
+            passportNumber: 'A4S5D67F0',
+            phoneCode: '55',
+            phoneNumber: '55119333333333'
+          },
+          {
+            _id: '4',
+            name: 'Doralice Cruz',
+            passportNumber: 'O65I4U3Y1',
+            phoneCode: '55',
+            phoneNumber: '55119444444444'
+          }
+        ],
+        forms: {
+          reserve: {
+            hasErrors: false,
+            iptClient: {
+              value: { _id: '' },
+              hasError: false,
+              error: ''
+            },
+            iptStartDate: {
+              value: '',
+              hasError: false,
+              error: ''
+            },
+            iptEndDate: {
+              value: '',
+              hasError: false,
+              error: ''
+            },
+            iptStatus: {
+              value: 'reservado',
+              hasError: false,
+              error: ''
+            }
+          },
+          searchClient: {
+            type: 'name',
+            hasErrors: false,
+            iptName: {
+              value: '',
+              hasError: false,
+              error: ''
+            },
+            iptCPF: {
+              value: '',
+              hasError: false,
+              error: ''
+            },
+            iptPassportNumber: {
+              value: '',
+              hasError: false,
+              error: ''
+            }
+          }
+        }
+      }
     },
     methods: {
       addUser() {
